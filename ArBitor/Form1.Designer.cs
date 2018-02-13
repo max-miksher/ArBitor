803587ad976711c916cdc902e9cfe5301d1d54d0
max-miksher <max-miksher@mail.ru>
2018-02-14 00:14:36 +0300

    Создание проекта
    Создание проекта

---
+++ ArBitor/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ArBitor
+{
+    partial class ArBitor
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ArBitor));
+            this.SuspendLayout();
+            // 
+            // ArBitor
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.MaximizeBox = false;
+            this.Name = "ArBitor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
