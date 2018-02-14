a8622f5ab226fd31a0950d49db065a763e4496ac
max-miksher <max-miksher@mail.ru>
2018-02-14 22:35:16 +0300

    Доработал синглтон для многопоточности. Убраллишний код

--- ArBitor/Form1.Designer.cs
+++ ArBitor/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -51,10 +52,16 @@
             resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
             // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
             // ArBitor
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -70,6 +77,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ArBitor/SystemDump.cs
+++ ArBitor/SystemDump.cs
@@ -10,24 +10,25 @@ namespace ArBitor
     public class SystemDump
     {
         private static SystemDump instance;
-        public DateTime a = new DateTime();
-        
+        private static readonly object obj = new object();
 
-        public SystemDump()
+        private SystemDump()
         {
-            a = DateTime.Now;
+            
         }
 
-        public static SystemDump getInstance()
+        public static SystemDump getInstance
         {
-            //if (instance == null)
-            // На всякий случай, ещё раз проверим не был
-            // ли объект создан другим потоком, пока
-            // текущий ждал освобождения блокировки.
-            if (instance == null)
-                instance = new SystemDump();
-
-            return instance;
+            get
+            {
+                if (instance == null)
+                    lock(obj)
+                    {
+                        if (instance == null)
+                            instance = new SystemDump();
+                    }
+                return instance;
+            }
         }
     }
 }
